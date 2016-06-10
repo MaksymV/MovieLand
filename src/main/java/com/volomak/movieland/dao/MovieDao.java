ca7b62ffe1d9d6b26a422f434a9de98a6211c4b3
MaksymV <maksvolo@gmail.com>
2016-06-10 17:33:36 +0300

    first steps of b1

--- src/main/java/com/volomak/movieland/dao/MovieDao.java
+++ src/main/java/com/volomak/movieland/dao/MovieDao.java
@@ -4,7 +4,7 @@ import com.volomak.movieland.entity.Movie;
 
 public interface MovieDao {
     Movie getById(int id);
-//1111111111111111111111111111
+//2222222222222222222222
     void add(Movie movie);
 
 }
