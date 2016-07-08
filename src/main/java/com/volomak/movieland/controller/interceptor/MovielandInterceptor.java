5fd85736c659c333cb6d04a80bbc5c208ccc1d01
MaksymV <maksvolo@gmail.com>
2016-07-08 08:11:15 +0300

    s1 and b5, second draft s2

--- src/main/java/com/volomak/movieland/controller/interceptor/MovielandInterceptor.java
+++ src/main/java/com/volomak/movieland/controller/interceptor/MovielandInterceptor.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -16,6 +17,7 @@ import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.UUID;
 
+@Component
 public class MovielandInterceptor extends HandlerInterceptorAdapter {
     private final Logger log = LoggerFactory.getLogger(getClass());
 
